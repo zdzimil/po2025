109d405177038bdfbebec33717c823a38080cf42
zdzimil <marcelzsem@gmail.com>
2025-10-06 16:15:20 +0200

    Lotto

---
+++ Lab1/Lotto.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class Lotto {
+ public static void main(String[] args) {
+	Int[] Numery = new Int[6];
+	Numery[0] = random.nextInt(50) + 1;
+	Numery[1] = random.nextInt(50) + 1;
+	Numery[2] = random.nextInt(50) + 1;	
+	Numery[3] = random.nextInt(50) + 1;
+	Numery[4] = random.nextInt(50) + 1;
+	Numery[5] = random.nextInt(50) + 1;
+ }
+}
