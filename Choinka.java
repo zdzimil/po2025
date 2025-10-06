b6f5b65287278800d307237b61bbf83d2c1dc221
zdzimil <marcelzsem@gmail.com>
2025-10-06 15:56:01 +0200

    Zajecia 1: Hello i Choinka

---
+++ Choinka.java
@@ -0,0 +1,11 @@
+public class Hello {
+ public static void main(String[] args) {
+	String galaz = "";
+	 for (int i = 1; i<= 10; i++) {
+		 
+		 galaz = galaz + "*";
+		 System.out.println(galaz);
+	 }
+	 
+ }
+}
