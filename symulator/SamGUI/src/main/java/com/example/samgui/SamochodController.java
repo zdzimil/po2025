781eba4d8c81dada63c786601fb39761c5d35d12
Marcel Kuzma <marcel@MacBook-Air-Marcel.local>
2025-11-24 17:30:13 +0100

    Podczas zajec

---
+++ symulator/SamGUI/src/main/java/com/example/samgui/SamochodController.java
@@ -0,0 +1,10 @@
+package com.example.samgui;
+
+import javafx.fxml.FXML;
+
+public class SamochodController {
+    @FXML
+    public void initialize() {
+        System.out.println("Kontroler załadowany poprawnie!");
+    }
+}
